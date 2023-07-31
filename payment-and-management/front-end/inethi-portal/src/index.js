a4266c4f7c2a6cfe59abfdc2abeca56f521070e4
Keegan White <keeganthomaswhite@gmail.com>
2023-07-31 13:00:04 +0200

    Updated to root.render in index.js and added service name to PurchaseHistory.js

--- payment-and-management/front-end/inethi-portal/src/index.js
+++ payment-and-management/front-end/inethi-portal/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import keycloak from './keycloak';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 
     <ReactKeycloakProvider authClient={keycloak}
             initOptions={{
@@ -19,8 +20,6 @@ ReactDOM.render(
         <App />
 
     </ReactKeycloakProvider>
-,
-  document.getElementById('root')
 );
 
 reportWebVitals();
--- payment-and-management/front-end/inethi-portal/src/pages/PurchaseHistory.js
+++ payment-and-management/front-end/inethi-portal/src/pages/PurchaseHistory.js
@@ -7,25 +7,65 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function PurchaseHistory() {
     const [payments, setPayments] = useState([]);
     const { keycloak } = useKeycloak();
+    const [services, setServices] = useState([]);
       axios.defaults.baseURL = 'http://0.0.0.0:8000';
       axios.defaults.headers.common['Authorization'] = `Bearer ${keycloak.token}`;
 
+
     useEffect(() => {
-        fetchPayments();
-    }, []);
+    // Fetch services
+    axios
+      .get("/get-services/")
+      .then(response => {
+        setServices(response.data);
+        // console.log(response.data)
+      })
+      .catch(error => {
+        console.error(`Error fetching services: ${error}`);
+      });
+}, []);
+
+    useEffect(() => {
+  // When services state updates and it's not an empty array, fetch payments
+  if(services.length > 0) {
+    fetchPayments(services).catch(error => console.error(`Error fetching payments: ${error}`));
+  }
+}, [services]);
+
+
 
     const fetchPayments = async () => {
-        try {
-            const response = await axios.get('/get-my-payments/', {
-                headers: {
-                    'Authorization': `Bearer ${keycloak.token}`
-                }
-            });
-            setPayments(response.data);
-        } catch (error) {
-            console.log('Error fetching payments', error);
-        }
-    };
+    try {
+        const response = await axios.get('/get-my-payments/', {
+            headers: {
+                'Authorization': `Bearer ${keycloak.token}`
+            }
+        });
+
+        // Map over each payment in the response
+        const updatedPayments = response.data.map(payment => {
+            // Find the service that has the same id as the service_id of the current payment
+            const service = services.find(service => service.id === payment.id);
+
+            // If the service is found, create a new object that has all properties of the current payment and an additional name property that has the value of service.name
+            if (service) {
+                // console.log('found matches')
+                return {
+                    ...payment,
+                    name: service.name
+                };
+            }
+
+            // If the service is not found, return the payment as is
+            return payment;
+        });
+        setPayments(updatedPayments);
+        // console.log(updatedPayments);
+    } catch (error) {
+        console.log('Error fetching payments', error);
+    }
+};
+
   const formatDateTime = datetime => {
     const options = {
       year: "numeric",
@@ -47,7 +87,7 @@ function PurchaseHistory() {
                 <table className="table table-striped">
                     <thead>
                         <tr>
-                            <th>Service ID</th>
+                            <th>Service Name</th>
                             <th>Amount</th>
                             <th>Date</th>
                             <th>Package</th>
@@ -57,7 +97,7 @@ function PurchaseHistory() {
                     <tbody>
                         {payments.map((payment, index) => (
                             <tr key={index}>
-                                <td>{payment.service_type_id}</td>
+                                <td>{payment.name}</td>
                                 <td>{payment.amount}</td>
                                 <td>{formatDateTime(payment.paydate_time)}</td>
                                 <td>{payment.package}</td>
