8a81387d40ab6352ab5a4bc6f558752d486331d5
Keegan White <keeganthomaswhite@gmail.com>
2023-09-14 16:21:20 +0200

    updated build for backend

--- payment-and-management/build.sh
+++ payment-and-management/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-cd backend || exit
-docker-compose build --no-cache || exit
-docker-compose config || exit
-docker-compose up -d || exit
-cd ../front-end/inethi-portal || exit
-docker-compose build --no-cache || exit
-docker-compose config || exit
-docker-compose up -d || exit
+cd backend || exit 1
+docker-compose build --no-cache || exit 1
+docker-compose config || exit 1
+docker-compose up -d inethi-user-management-mysql || exit 1
+echo Waiting for MySQL container to initialise...
+sleep 30
+docker-compose up -d inethi-user-managementl || exit 1
+inethi-user-management
+cd ../front-end/inethi-portal || exit 1
+docker-compose build --no-cache || exit 1
+docker-compose config || exit 1
+docker-compose up -d || exit 1
 echo DONE
-sleep 5
